c9f11c88a6a227be27c7655d22cbec32b7fb1a82
LE DAT <dat2111993@gmail.com>
2020-08-21 14:55:50 +0700

    update

--- REACT/amazon/src/App.js
+++ REACT/amazon/src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router,Switch, Route,Link} from "react-router-dom";
+import Header from './component/Header';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-     Test
+      <Switch>
+        
+        <Route path="/checkout">
+          <h1>Checkout</h1>
+        </Route>
+
+        <Route path="/login">
+          <h1>LOGIN</h1>
+        </Route>
+
+        <Route path="/">
+          <Header />
+          <h1>HOME PAGE</h1>
+         
+        </Route>
+
+
+      </Switch>
     </div>
+    </Router>
   );
 }
 
